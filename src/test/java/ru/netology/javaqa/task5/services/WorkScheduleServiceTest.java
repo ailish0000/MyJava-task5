ab37add4dfe42219875023caf2d747744a1caa0a
natakumtest <nkumasinskaya@gmail.com>
2024-12-29 00:37:33 +0300

    Add parametrized test

--- src/test/java/ru/netology/javaqa/task5/services/WorkScheduleServiceTest.java
+++ src/test/java/ru/netology/javaqa/task5/services/WorkScheduleServiceTest.java
@@ -2,43 +2,17 @@ package ru.netology.javaqa.task5.services;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class WorkScheduleServiceTest {
 
-    @Test
-    void shouldCalculateThreeMonths() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/test.csv")
+
+    void shouldCalculateMonths( int income, int expenses, int threshold, int expected) {
         WTService service = new WTService();
-
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
-
-        int actual = service.calcWork(income, expenses, threshold);
-        Assertions.assertEquals(expected, actual);
-    }
-
-    @Test
-    void shouldCalculateZeroMonths() {
-        WTService service = new WTService();
-
-        int income = 50_000;
-        int expenses = 40_000;
-        int threshold = 150_000;
-        int expected = 0;
-
-        int actual = service.calcWork(income, expenses, threshold);
-        Assertions.assertEquals(expected, actual); // Проверяем результат
-    }
-
-    @Test
-    void shouldCalculateFourMonths() {
-        WTService service = new WTService();
-
-        int income = 100_000;
-        int expenses = 10_000;
-        int threshold = 150_000;
-        int expected = 4;
 
         int actual = service.calcWork(income, expenses, threshold);
         Assertions.assertEquals(expected, actual);
